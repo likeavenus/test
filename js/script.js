1216049cf0ba8d68474388db42d1f3c2eab5d61a
Rafael Shepard <rafael_gulie94@list.ru>
2018-04-25 21:26:10 +0300

    п

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   const toggle = $('.header__list')
   $('.adapt-header__burger').click(function(){
-      toggle.toggleClass('header__list--show');
+      toggle.slideToggle('header__list--show');
     });
   });
